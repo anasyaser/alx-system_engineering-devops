00a0651c31753be0312b9d2ec8c13214dfdb6e9d
anasyaser <anoos649@gmail.com>
2024-01-31 14:26:35 +0200

    Fix style issue

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,10 +6,12 @@ import sys
 if __name__ == "__main__":
     request = requests.Session()
     url_1 = "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
-    url_2 = "https://jsonplaceholder.typicode.com/user/{}/todos".format(sys.argv[1])
+    url_2 = "https://jsonplaceholder.typicode.com/user/{}/todos".format(
+        sys.argv[1])
     user_info = request.get(url_1).json()
     user_tasks = request.get(url_2).json()
-    completed_tasks = [item['title'] for item in user_tasks if item['completed']]
+    completed_tasks = [item['title'] for item in user_tasks
+                       if item['completed']]
     employee_name = user_info["name"]
     done_tasks = len(completed_tasks)
     total_tasks = len(user_tasks)
