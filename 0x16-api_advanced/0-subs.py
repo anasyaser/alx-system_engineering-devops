36490b6391204963864be84f74889f776f564020
anas <anoos649@gmail.com>
2024-02-13 21:06:28 +0200

    Fix style issue

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,9 +6,10 @@ import sys
 
 def number_of_subscribers(subreddit):
     """Fetch subreddit subscribers"""
-    url = "https://www.reddit.com/r/{}/about.json".format(sys.argv[1]);
+    url = "https://www.reddit.com/r/{}/about.json".format(sys.argv[1])
 
-    response = requests.get(url)
+    response = requests.get(url, allow_redirects=False,
+                            headers={"User-Agent": "custom"})
 
     if (response.status_code == 200):
         return response.json().get("data").get("subscribers")
