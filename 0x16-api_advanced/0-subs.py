6970368e8a12362982c9f90fa2ac1cbe7b254a3f
anas <anoos649@gmail.com>
2024-02-13 21:24:09 +0200

    Top ten posts

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
     response = requests.get(url, allow_redirects=False,
-                            headers={"User-Agent": "custom"})
+                            headers={"User-Agent": "Custom"})
 
     if (response.status_code == 200):
         return response.json().get("data").get("subscribers")
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Get the top ten posts for given subreddit"""
+import requests
+
+
+def top_ten(subreddit):
+    """print subreddit top ten posts"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+    response = requests.get(url, allow_redirects=False,
+                            headers={"User-Agent": "custom"},
+                            params={"limit": 10})
+
+    if response.status_code == 200:
+        posts = response.json().get("data").get("children")
+        for post in posts:
+            print(post.get("data").get("title"))
+    else:
+        print(None)
