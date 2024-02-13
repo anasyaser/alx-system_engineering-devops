08f2a51ebca284d5edd5ecdd922bf8dda2b24a42
anas <anoos649@gmail.com>
2024-02-13 21:04:15 +0200

    Get subriddet subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 """Fetch api subscripbers for subreddit"""
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    
+    """Fetch subreddit subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(sys.argv[1]);
+
+    response = requests.get(url)
+
+    if (response.status_code == 200):
+        return response.json().get("data").get("subscribers")
+    else:
+        return 0
