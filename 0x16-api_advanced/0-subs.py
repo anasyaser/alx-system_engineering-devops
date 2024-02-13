a6e01fa50a4cef24e7f71fcc59231359b1aa64e7
anas <anoos649@gmail.com>
2024-02-13 21:08:19 +0200

    Fix typo issues

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """Fetch api subscripbers for subreddit"""
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
     """Fetch subreddit subscribers"""
-    url = "https://www.reddit.com/r/{}/about.json".format(sys.argv[1])
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
     response = requests.get(url, allow_redirects=False,
                             headers={"User-Agent": "custom"})
