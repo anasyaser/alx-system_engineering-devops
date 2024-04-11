6128c0e5d57d1a6ab288a9667042f36c1c25fd71
anas <anoos649@gmail.com>
2024-04-11 16:08:34 +0200

    Fix style issue

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,8 +10,11 @@ def number_of_subscribers(subreddit):
     """Fetch subreddit subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-    response = requests.get(url, allow_redirects=False,
-                            headers={"User-Agent": "Custom"})
+    response = requests.get(
+        url,
+        allow_redirects=False,
+        headers={"User-Agent": "Custom"},
+    )
 
     if (response.status_code == 200):
         return response.json().get('data').get("subscribers")
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,9 +10,12 @@ def top_ten(subreddit):
     """print subreddit top ten posts"""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
 
-    response = requests.get(url, allow_redirects=False,
-                            headers={"User-Agent": "custom"},
-                            params={"limit": 10})
+    response = requests.get(
+        url,
+        allow_redirects=False,
+        headers={"User-Agent": "custom"},
+        params={"limit": 10},
+    )
 
     if response.status_code == 200:
         posts = response.json().get("data").get("children")
