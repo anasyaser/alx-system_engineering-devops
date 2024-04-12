6ecadf91544bef3dcd88ddcd8c77564d4ed651cd
anas <anoos649@gmail.com>
2024-04-12 10:29:06 +0200

    Fix style issue

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fix apache2 server error
 
-exec {"Fix apache2 server":
+exec { "Fix apache2 server":
   provider => 'shell',
   command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
